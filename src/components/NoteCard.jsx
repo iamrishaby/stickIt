393b3032a1e87a7843cfd22a14809c06542f5b5b
Rishab Yadav <rishab130402@gmail.com>
2024-07-24 04:11:00 +0530

    initial commit

---
+++ src/components/NoteCard.jsx
@@ -0,0 +1,36 @@
+//import React from "react";
+
+import Trash from "../icons/Trash";
+const NoteCard = ({ note }) => {
+    const body = JSON.parse(note.body);
+    const position = JSON.parse(note.position);
+    const colors = JSON.parse(note.colors);
+ 
+    return (
+        <div
+            className="card"
+            style={{
+                backgroundColor: colors.colorBody,
+                left: '${position.x}px',
+                top: '${position.y}px',
+                
+            }}
+        >
+            <div
+                className="card-header"
+                style={{ backgroundColor: colors.colorHeader }}
+    >
+
+        <Trash />
+    </div>
+
+            <div className="card-body">
+                <textarea style={{ color: colors.colorText }}
+                defaultValue={body}
+                ></textarea>
+                </div>
+        </div>
+    );
+};
+
+export default NoteCard;
